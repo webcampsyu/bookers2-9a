90badbd05c60ba64c61f51f9fa9e0ee56969af5c
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-06-09 02:32:35 +0000

    [finish]閲覧数表示(表示位置修正)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end 
     @books = Book.new
     @user = @book.user
     @book_comment = BookComment.new
-    @book_detail = Book.find(params[:id])
-    unless ViewCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
-      current_user.view_counts.create(book_id: @book_detail.id)
-    end 
+    
   end
 
   def index
